f5f032c34b43d30dfb648605774ca65e3c3eafc3
Visava <onisawavisava@gmail.com>
2018-11-26 22:00:49 +0700

    add select library action

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
-import { CardSection } from './common';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { connect } from 'react-redux';
 
+import { CardSection } from './common';
 import color from '../constant/color.json'
+import * as actions from '../actions';
 
 class ListItem extends Component {
+  renderDescription() {
+    const { library, selectedLibraryId } = this.props
+
+    if (library.id === selectedLibraryId) {
+      return (
+        <Text>{library.description}</Text>
+      );
+    }
+  }
+
   render() {
     const { titleStyle } = styles
+    const { id, title } = this.props.library
     return (
-      <CardSection>
-        <Text style={titleStyle}>{this.props.library.title}</Text>
-      </CardSection>
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>
+              {title}
+            </Text>
+          </CardSection>
+          {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -21,6 +43,10 @@ const styles = {
     paddingLeft: 15,
     color: color.text
   }
-}
+};
 
-export default ListItem
+const mapStateToProps = (state) => ({
+  selectedLibraryId: state.selectedLibraryId
+});
+
+export default connect(mapStateToProps, actions)(ListItem);
