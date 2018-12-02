349237103b28395ab2dde9a3e6e9039dfdc30457
Visava <onisawavisava@gmail.com>
2018-12-02 12:03:09 +0700

    add confirm modal

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -4,13 +4,15 @@ import _ from 'lodash';
 import { text } from 'react-native-communications';
 
 import { employeeUpdate, employeeEdit } from '../actions';
-import { Card, CardSection, Button } from './common';
+import { Card, CardSection, Button, ConfirmModal } from './common';
 import EmployeeForm from './EmployeeForm';
 
 import t from '../constant/text.json';
 import { shiftDay } from '../constant/day';
 
 class EmployeeEditPage extends Component {
+  state = { showModal: false };
+
   componentWillMount() {
     _.each(this.props.employee, (value, prop) => {
       this.props.employeeUpdate({ prop, value });
@@ -49,6 +51,18 @@ class EmployeeEditPage extends Component {
             {t.texting_btn}
           </Button>
         </CardSection>
+
+        <CardSection>
+          <Button onPress={() => this.setState({ showModal: !this.state.showModal })}>
+            {t.delete_btn}
+          </Button>
+        </CardSection>
+
+        <ConfirmModal
+          visible={this.state.showModal}
+        >
+          {t.delete_confirm}
+        </ConfirmModal>
       </Card>
     );
   }
