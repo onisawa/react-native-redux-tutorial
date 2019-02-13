1747e09d651935816c3a25d127d745643cd92cbb
Big <visava@simyasolutions.com>
2019-02-13 22:27:57 +0700

    refactor store

--- src/actions/AuthActions.spec.js
+++ src/actions/AuthActions.spec.js
@@ -1,21 +1,33 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 
-import { selectLibrary } from './AuthActions';
-import { SELECT_LIBRARY } from './types';
+import { selectLibrary, emailChanged } from './AuthActions';
+import { SELECT_LIBRARY, EMAIL_CHANGED } from './types';
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
+const store = mockStore();
+
 describe('Auth Actions', () => {
   it('select library', () => {
-    const store = mockStore();
     store.dispatch(selectLibrary(1));
 
     const actions = store.getActions();
     expect(actions).toContainEqual({
       type: SELECT_LIBRARY,
       payload: 1,
+    });
+  });
+
+  it('email changed', () => {
+    const text = 'email';
+    store.dispatch(emailChanged(text));
+
+    const actions = store.getActions();
+    expect(actions).toContainEqual({
+      type: EMAIL_CHANGED,
+      payload: text,
     });
   });
 });
